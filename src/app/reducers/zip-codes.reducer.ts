f5be970d0db4be396314672fb4b4affc7e12eacc
Alain Chautard <alain@interstate21.com>
2018-06-13 11:00:49 -0700

    Implemented reducer code (video 2.3)

--- src/app/reducers/zip-codes.reducer.ts
+++ src/app/reducers/zip-codes.reducer.ts
@@ -1,4 +1,4 @@
-import { Action } from '@ngrx/store';
+import {ZipcodeActions, ZipcodeActionTypes} from '../actions/zipcode.actions';
 
 
 export interface State {
@@ -9,10 +9,13 @@ export const initialState: State = {
     zipcodes: []
 };
 
-export function reducer(state = initialState, action: Action): State {
+export function reducer(state = initialState, action: ZipcodeActions): State {
   switch (action.type) {
-
-    default:
+      case ZipcodeActionTypes.AddZipcode:
+        return {...state, zipcodes: [...state.zipcodes, action.zipcode]};
+      case ZipcodeActionTypes.RemoveZipcode:
+          return {...state, zipcodes: state.zipcodes.filter( item => item !== action.zipcode)};
+      default:
       return state;
   }
 }
